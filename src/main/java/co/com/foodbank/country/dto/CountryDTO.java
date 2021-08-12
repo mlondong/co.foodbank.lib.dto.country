fd76d25feedddfab664793d49e9237dd986b3f44
mlondong <mauricio.londono@gmail.com>
2021-08-11 21:47:16 -0500

    implemented

--- src/main/java/co/com/foodbank/country/dto/CountryDTO.java
+++ src/main/java/co/com/foodbank/country/dto/CountryDTO.java
@@ -2,31 +2,18 @@ package co.com.foodbank.country.dto;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * @author mauricio.londono@gmail.com co.com.foodbank.country.dto 16/05/2021
  */
+@Data
+@NoArgsConstructor
 public class CountryDTO {
 
     @NotNull
     @NotBlank(message = "Country must be completed.")
     public String name;
-
-
-    public CountryDTO() {}
-
-    public CountryDTO(String name) {
-        super();
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
 
 }
